32e1c4b69e7afa5c1891042df9e354c9c1e50ad4
Aki Joensuu <aki@akikuvaa.com>
2025-04-08 01:54:44 +0300

    Update Next.js and Netlify config for static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,7 @@ const nextConfig = {
         pathname: '**',
       },
     ],
+    unoptimized: true,
   },
 }
 
