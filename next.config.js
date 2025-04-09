bed51ee7d4cd49c79ce286fe13ec4d01f70f355e
Aki Joensuu <aki@akikuvaa.com>
2025-04-09 11:47:42 +0300

    Add remotePatterns for Sanity images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '**',
+      },
+    ],
   },
 }
 
